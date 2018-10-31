6397974f6b714168bc53ae2432651a5fea94c351
Mabel Cao <10001009@student.toiohomai.ac.nz>
2018-10-31 14:17:20 +1300

    flow_diagrams

---
+++ Test1/Test1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please enter in a starting number");
+            int a = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter in the end number");
+            int b = Int32.Parse(Console.ReadLine());
+
+            int sum = 0;
+
+            for (int i = a; i <= b; i++)
+            {
+                sum += i ;
+            }
+            Console.WriteLine($"{sum}");
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Test2/Test2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("This is a basic menu system");
+            Console.WriteLine("Please enter in a number from 1 to 4, for a task");
+            Console.WriteLine("1 for Option 1");
+            Console.WriteLine("2 for Option 2");
+            Console.WriteLine("3 for Option 3");
+            Console.WriteLine("4 to exit application");
+
+            int choice = Int32.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Option 1");
+                    break;
+                case 2:
+                    Console.WriteLine("Option 2");
+                    break;
+                case 3:
+                    Console.WriteLine("Option 3");
+                    break;
+                case 4:
+                    Console.WriteLine("Exit application");
+                    break;
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
